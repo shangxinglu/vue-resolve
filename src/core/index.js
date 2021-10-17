35814af2f3f03dbc69f2d53a6e11194f0da18cd7
shangxinglu <2609209328@qq.com>
2021-10-17 09:54:54 +0800

    init

---
+++ src/core/index.js
@@ -0,0 +1,26 @@
+import Vue from './instance/index' // 加载Vue函数
+import { initGlobalAPI } from './global-api/index' // 全局初始化API
+import { isServerRendering } from 'core/util/env'
+import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
+
+initGlobalAPI(Vue)
+
+Object.defineProperty(Vue.prototype, '$isServer', {
+  get: isServerRendering
+})
+
+Object.defineProperty(Vue.prototype, '$ssrContext', {
+  get () {
+    /* istanbul ignore next */
+    return this.$vnode && this.$vnode.ssrContext
+  }
+})
+
+// expose FunctionalRenderContext for ssr runtime helper installation
+Object.defineProperty(Vue, 'FunctionalRenderContext', {
+  value: FunctionalRenderContext
+})
+
+Vue.version = '__VERSION__'
+
+export default Vue
